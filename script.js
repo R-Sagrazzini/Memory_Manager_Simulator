58d5f6c36904e4d46c4a90428971bae5972178df
R-Sagrazzini <richysag22@gmail.com>
2026-02-11 09:44:52 +0100

    Inizio programma

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-function setup(){
-    createCanvas(400, 400);
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('p5Canvas');
     background(220);
-}
-function draw(){
-    noLoop();
-    fill("lightblue");
-    rect(10, 10, 100, 40);
-    rect(10, 70, 100, 40);
-    rect(10, 130, 100, 40);
-    fill("lightgreen");
-    rect(290, 10, 100, 40);
-    rect(290, 70, 100, 40);
-    rect(290, 130, 100, 40);
-
-    stroke('red');
-    line(110, 30, 290, 30);
-    line(110, 90, 290, 90);
-    line(110, 150, 290, 150);
-
-    
 }
