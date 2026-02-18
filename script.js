7f297492542d5b0517ae4597d9049186a474b46f
R-Sagrazzini <richysag22@gmail.com>
2026-02-18 09:47:22 +0100

    Grafica e allineamentiù

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+//Memory Manager System
+//Algoritmo di paginazione
+//Memory Manager System
+//Algoritmo di paginazione
+
+let canvasContainer;
+
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight);
-    canvas.parent('p5Canvas');
-    background(220);
+  canvasContainer = document.getElementById('p5Canvas');
+  
+  // Crea canvas con larghezza del contenitore
+  let w = canvasContainer.offsetWidth;
+  let h = w * 0.55; // aspect ratio
+  
+  var canvas = createCanvas(w, h);
+  canvas.parent('p5Canvas');
+}
+
+function windowResized() {
+  // Ridimensiona quando cambia la finestra
+  let w = canvasContainer.offsetWidth;
+  let h = w * 0.55;
+  resizeCanvas(w, h);
+}
+
+function draw() {
+  background("#004f59");
+  rect(10, 20, width - 20, height - 40);
 }
